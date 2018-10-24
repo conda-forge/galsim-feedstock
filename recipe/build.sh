d42d53d2100dd9f60aa9a4d91729cfef8333fcb8
Matthew R Becker <becker.mr@gmail.com>
2018-10-24 07:28:33 -0700

    put in a test

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-alias c++=x86_64-conda_cos6-linux-gnu-c++
+if [[ -z `which c++` ]]
+then
+    alias c++=x86_64-conda_cos6-linux-gnu-c++
+fi
 
 ${PYTHON} -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
