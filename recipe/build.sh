b6d188247ed5928cac26a03fd573509d04c36eff
Brian Van Klaveren <bvan@slac.stanford.edu>
2019-09-19 09:22:23 -0700

    Fix so SConstruct sniffs out g++

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,7 @@
+if [ -z "${GXX}" ]; then
+    # SConsstruct wants to find 'g++' in name
+    export CXX=${GXX}
+fi
 export SCONSFLAGS="PYBIND11_DIR=$PREFIX EIGEN_DIR=$PREFIX/include/eigen3"
 scons CXX=${CXX}
 scons install
-
