dbfad86d91d3e393422d74a400ae115bbefa1247
Brian Van Klaveren <bvan@slac.stanford.edu>
2019-09-19 14:47:05 -0700

    Use pybind11 2.2.4 and add build dependencies

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,8 @@
-SCONSFLAGS+=" USE_UNKNOWN_VARS=true"
 if [ -n "${GXX}" ]; then
     # SConstruct wants to find 'g++' in name
     CXX=${GXX}
 fi
 export SCONSFLAGS="PREFIX=$PREFIX PYPREFIX=$PREFIX/lib/python PYBIND11_DIR=$PREFIX EIGEN_DIR=$PREFIX/include/eigen3 CXX=$CXX"
+
 scons
 scons install
