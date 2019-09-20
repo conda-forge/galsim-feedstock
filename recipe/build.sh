293a8806e2c8997654d9cde9c80773b681ee3b2b
Brian Van Klaveren <bvan@slac.stanford.edu>
2019-09-20 09:08:55 -0700

    Rework SConsflags for DYLD_LIBRARY_PATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,16 @@
+SCONFSFLAGS=""
 if [ -n "${GXX}" ]; then
     # SConstruct wants to find 'g++' in name, and conda will have gnu-c++ in name
-    CXX=${GXX}
+    SCONSFLAGS+="CXX=${GXX} "
 fi
-# Tell it the prefix and compiler
-SCONSFLAGS+="PREFIX=$PREFIX CXX=$CXX "
+
+if [[ $OSTYPE == darwin* ]]; then
+    # Darwin
+    SCONSFLAGS+="CXX=${CLANGXX} DYLD_LIBRARY_PATH=$PREFIX/lib "
+fi
+
+# Tell it the prefix
+SCONSFLAGS+="PREFIX=$PREFIX "
 # Don't pick up conda _build_env python
 SCONSFLAGS+="PYTHON=$PREFIX/bin/python "
 # Manually tell it where fftw/pybind11/eigen headers are and export
