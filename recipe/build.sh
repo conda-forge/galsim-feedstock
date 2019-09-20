ce066aa66d0bc27fe552cc65fc42a4e4b21de43e
Brian Van Klaveren <bvan@slac.stanford.edu>
2019-09-20 08:44:46 -0700

    Add /lib to end of FFTW_DIR var

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ SCONSFLAGS+="PREFIX=$PREFIX CXX=$CXX "
 # Don't pick up conda _build_env python
 SCONSFLAGS+="PYTHON=$PREFIX/bin/python "
 # Manually tell it where fftw/pybind11/eigen headers are and export
-export SCONSFLAGS+="FFTW_DIR=$PREFIX PYBIND11_DIR=$PREFIX EIGEN_DIR=$PREFIX/include/eigen3 "
+export SCONSFLAGS+="FFTW_DIR=$PREFIX/lib PYBIND11_DIR=$PREFIX EIGEN_DIR=$PREFIX/include/eigen3 "
 
 scons
 scons install
