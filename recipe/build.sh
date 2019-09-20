535cbcb32fea3753399671d6852cc8086bdb5720
Brian Van Klaveren <bvan@slac.stanford.edu>
2019-09-20 08:29:48 -0700

    Manually add dependencies to SCONSFLAGGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,8 @@ fi
 SCONSFLAGS+="PREFIX=$PREFIX CXX=$CXX "
 # Don't pick up conda _build_env python
 SCONSFLAGS+="PYTHON=$PREFIX/bin/python "
-# Manually tell it where eigen headers are and export
-export SCONSFLAGS+="EIGEN_DIR=$PREFIX/include/eigen3 "
+# Manually tell it where fftw/pybind11/eigen headers are and export
+export SCONSFLAGS+="FFTW_DIR=$PREFIX PYBIND11_DIR=$PREFIX EIGEN_DIR=$PREFIX/include/eigen3 "
 
 scons
 scons install
