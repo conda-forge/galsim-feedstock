00256fbc7f6e5e977abd734a9a260f48c8d54bdf
Eli Rykoff <erykoff@stanford.edu>
2021-01-18 21:49:17 -0800

    Add cross-compiling and scons 4 support.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,20 @@
-SCONFSFLAGS=""
+#!/bin/bash
+
+set -x
+
+SCONSFLAGS=""
 if [ -n "${GXX}" ]; then
     # SConstruct wants to find 'g++' in name, and conda will have gnu-c++ in name
     SCONSFLAGS+="CXX=${GXX} "
 fi
 
-if [[ $OSTYPE == darwin* ]]; then
-    # Darwin
+if [[ $target_platform == osx-* ]]; then
+    # macos
     SCONSFLAGS+="CXX=${CLANGXX} DYLD_LIBRARY_PATH=$PREFIX/lib "
+fi
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+    SCONSFLAGS+="CROSS_COMPILING=1 "
 fi
 
 # Tell it the prefix
@@ -16,7 +24,7 @@ SCONSFLAGS+="PYTHON=$PREFIX/bin/python "
 # Manually tell it where fftw/pybind11/eigen headers are and export
 export SCONSFLAGS+="FFTW_DIR=$PREFIX/lib PYBIND11_DIR=$PREFIX EIGEN_DIR=$PREFIX/include/eigen3 "
 
-scons
-scons install
+python `which scons`
+python `which scons` install
 cp include/GalSim.h "$PREFIX/include"
 cp -r include/galsim "$PREFIX/include"
