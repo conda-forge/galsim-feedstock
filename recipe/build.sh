20b7e43a09d0de2927b08f49389c56a41735e899
Eli Rykoff <erykoff@stanford.edu>
2021-07-05 21:04:37 -0700

    Add symlinks to versioned library.

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,3 +8,10 @@ python setup.py build_shared_clib
 cp include/GalSim.h "$PREFIX/include"
 cp -r include/galsim "$PREFIX/include"
 cp build/shared_clib/libgalsim* "$PREFIX/lib"
+
+# Create symlinks to the versioned library
+if [[ "$target_platform" =~ osx-* ]]; then
+  ln -s $PREFIX/lib/libgalsim.*.dylib $PREFIX/lib/libgalsim.dylib
+else
+  ln -s $PREFIX/lib/libgalsim.*.so $PREFIX/lib/libgalsim.so
+fi
