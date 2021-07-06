f15564f4528b392473678456172eb270cf4fd36d
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-07-06 14:48:01 -0500

    Update recipe/build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-python -m pip install . -vv
-python setup.py build_shared_clib
+${PYTHON} -m pip install . -vv
+${PYTHON} setup.py build_shared_clib
 
 cp include/GalSim.h "$PREFIX/include"
 cp -r include/galsim "$PREFIX/include"
