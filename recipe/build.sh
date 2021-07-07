028b0667a57b37a49f284f8574708fd24bdd3ed4
Eli Rykoff <erykoff@stanford.edu>
2021-07-06 22:14:27 -0700

    Update to 2.3.1 and fix build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,18 +2,9 @@
 
 set -x
 
-export LDFLAGS="$LDFLAGS -lfftw3 -fopenmp -lstdc++ -lm"
-
 ${PYTHON} -m pip install . -vv --install-option="-j${CPU_COUNT}"
 ${PYTHON} setup.py build_shared_clib -j${CPU_COUNT}
 
 cp include/GalSim.h "$PREFIX/include"
 cp -r include/galsim "$PREFIX/include"
-cp build/shared_clib/libgalsim* "$PREFIX/lib"
-
-# Create symlinks to the versioned library
-if [[ "$target_platform" =~ osx-* ]]; then
-  ln -s $PREFIX/lib/libgalsim.*.dylib $PREFIX/lib/libgalsim.dylib
-else
-  ln -s $PREFIX/lib/libgalsim.*.so $PREFIX/lib/libgalsim.so
-fi
+cp -PR build/shared_clib/libgalsim* "$PREFIX/lib"
