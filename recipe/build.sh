934d4eb366eb8f2dfa9197e9f7e5a727fc6a227c
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-07-23 20:39:28 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,3 +10,10 @@ ${PYTHON} setup.py build_shared_clib -j${CPU_COUNT}
 cp include/GalSim.h "$PREFIX/include"
 cp -r include/galsim "$PREFIX/include"
 cp -PR build/shared_clib/libgalsim* "$PREFIX/lib"
+
+if [[ ${target_platform} == osx-* ]]; then
+    lname=$(basename ${PREFIX}/lib/libgalsim.*.dylib)
+    echo "changing id of ${lanem} w/ install_name_tool"
+
+    install_name_tool -id "@rpath/${lname}" ${lname}
+fi
