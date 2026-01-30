363f861775d2a966fdd586442a3e47685a6b6b63
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-01-30 05:41:01 -0600

    Update build.sh to remove parallel build options
    Removed the '-j${CPU_COUNT}' option from pip install and setup.py commands.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,8 @@ set -x
 
 export FFTW_DIR="$PREFIX/lib"
 
-${PYTHON} -m pip install . -vv --global-option="-j${CPU_COUNT}"
-${PYTHON} setup.py build_shared_clib -j${CPU_COUNT}
+${PYTHON} -m pip install . -vv
+${PYTHON} setup.py build_shared_clib
 
 cp include/GalSim.h "$PREFIX/include"
 cp -r include/galsim "$PREFIX/include"
